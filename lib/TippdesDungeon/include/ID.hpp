bc7202e172da3fa8410cad5ff6ccf0c86dc07c47
paul <54645365+koplas@users.noreply.github.com>
2021-01-19 18:29:59 +0100

    Extend tiles

--- lib/TippdesDungeon/include/ID.hpp
+++ lib/TippdesDungeon/include/ID.hpp
@@ -5,11 +5,31 @@
 #define FLOOR 1
 #define PLAYER 2
 #define WALL 3
+#define DOOR 4
+#define DEAD 5
+#define SWORD 6
+
 #define ENEMY_0 100
+#define ENEMY_1 101
+#define ENEMY_2 102
+
+#define COIN_0 200
+#define COIN_1 201
+#define POTION_0 202
 
 // Define image path
 #define PLAYER_TILE "./img/player.svg"
 #define WALL_TILE "./img/wall.svg"
-#define ENEMY_0_TILE "./img/enemy_0.svg"
+#define DOOR_TILE "./img/door.svg"
+#define DEAD_TILE "./img/dead.svg"
+#define SWORD "./img/sword.svg"
 
-#endif // ID_H
+#define ENEMY_0_TILE "./img/enemy_0.svg"
+#define ENEMY_1_TILE "./img/enemy_1.svg"
+#define ENEMY_2_TILE "./img/enemy_2.svg"
+
+#define COIN_0_TILE "./img/coin_0.svg"
+#define COIN_1_TILE "./img/coin_1.svg"
+#define POTION_0_TILE "./img/potion_0.svg"
+
+#endif  // ID_H
